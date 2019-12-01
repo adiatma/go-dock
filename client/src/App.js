fd95db8f5e96760472a222451f80ba16a8955d61
adiatma <adiatma.kamaarudin@ralali.com>
2019-12-02 01:19:13 +0700

    feat: add home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { StoreProvider } from 'easy-peasy';
+
+import store from './store';
+import Images from './Images';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StoreProvider store={store}>
+      <Images />
+    </StoreProvider>
   );
 }
 
