400286f872857438ccec88dafa5c38c85a56f1a2
adiatma <adiatma.kamaarudin@ralali.com>
2019-12-02 23:03:56 +0700

    chore: improve ui

--- client/src/components/TopNavigation.js
+++ client/src/components/TopNavigation.js
@@ -1,25 +1,39 @@
 // @flow
-import * as React from 'react';
-import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem } from 'reactstrap';
-import { Link } from 'react-router-dom'
+import * as React from 'react'
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  Container,
+  NavLink
+} from 'reactstrap'
 
 function TopNavigation(): React.Node {
   const [isOpen, setIsOpen] = React.useState(false)
   const toggle = () => setIsOpen(!isOpen)
 
   return (
-    <div style={{ marginBottom: "1rem" }}>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">Go Dock</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <Link to={`/images`} style={{ textDecoration: 'none', color: 'black' }}>Images</Link>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
+    <div style={{marginBottom: '1rem'}}>
+      <Container>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Go Dock</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink
+                  href={`https://github.com/adiatma/go-dock`}
+                >
+                  Github
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </Container>
     </div>
   )
 }
