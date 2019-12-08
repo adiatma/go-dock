8fc024e5e452dbb534c52c183daebebfe136093e
adiatma <adiatma.kamaarudin@ralali.com>
2019-12-08 19:44:32 +0700

    chore: add build assets

---
+++ client/build/precache-manifest.6f4200e82fff823dc9ef1298b5a4e4eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc82d0609594c1c9756f5dcf96c832e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9b59b96676e4d140845",
+    "url": "/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "d9b59b96676e4d140845",
+    "url": "/static/js/2.cad07aaa.chunk.js"
+  },
+  {
+    "revision": "d723183a8a1948a1b5eb",
+    "url": "/static/js/3.55fb9b4b.chunk.js"
+  },
+  {
+    "revision": "786db7b805d85b68ab9d",
+    "url": "/static/js/4.4a7c6611.chunk.js"
+  },
+  {
+    "revision": "f87d680cf56b8f34293d",
+    "url": "/static/js/main.cad2576f.chunk.js"
+  },
+  {
+    "revision": "bf7a36ed85d52d894846",
+    "url": "/static/js/runtime-main.d4addc6a.js"
+  }
+]);
